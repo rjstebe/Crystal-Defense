d8533b8c530d758219997c72426ed6f5fd95037d
Felix Chen <felixchen20@gmail.com>
2022-02-21 21:08:07 -0500

    working sprites

--- game.cpp
+++ game.cpp
@@ -7,11 +7,22 @@
 #include "GameManager.h"
 #include "ResourceManager.h"
 #include "Object.h"
+#include "Hero.h"
+#include "Vector.h"
+#include "Crystal.h"
+#include "Bullet.h"
+#include "Wall.h"
+#include "Enemy.h"
 
 int main(int argc, char* argv[]) {
     GM.startUp();
     LM.setFlush();
     RM.loadSprite("sprites/pole.txt", "pole");
+    RM.loadSprite("sprites/hero-spr.txt", "hero");
+    RM.loadSprite("sprites/crystal-spr.txt", "crystal");
+    RM.loadSprite("sprites/enemy-spr.txt", "enemy");
+    RM.loadSprite("sprites/bullet-spr.txt", "bullet");
+    RM.loadSprite("sprites/wall-spr.txt", "wall");
     
     class TestObj : public df::Object {
     public:
@@ -21,6 +32,11 @@ int main(int argc, char* argv[]) {
         }
     };
     new TestObj;
+    new Hero;
+    new Crystal;
+    new Enemy;
+    new Bullet(df::Vector(0, 0));
+    new Wall;
     GM.run();
 
     GM.shutDown();
