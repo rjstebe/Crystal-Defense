e018bfb0fc6f112c084ba256eeb45495f0e2e662
Ryan Stebe <rjstebe1@gmail.com>
2022-02-28 17:52:45 -0500

    Fixed wall collision box

--- Wall.cpp
+++ Wall.cpp
@@ -8,7 +8,7 @@ Wall::Wall(df::Vector side1, df::Vector side2)
 	this->end2 = side2;
 	setSprite("wall");
 	if (this->end1.getY() - this->end2.getY() == 0) {
-		length = abs(this->end1.getX() - this->end2.getX());
+		length = abs(this->end1.getX() - this->end2.getX()) + 1;
 		dir = 1; // horizontal wall
 		if (end1.getX() > end2.getX()) {
 			this->end2.setX(side1.getX());
@@ -16,16 +16,16 @@ Wall::Wall(df::Vector side1, df::Vector side2)
 		}
 		LM.writeLog("end1 %f", end1.getX());
 		LM.writeLog("length %i", this->length);
-		setBox(df::Box(pixelsToSpaces(this->end1 - df::Vector(10, 10)), this->length-0.5, 0.5));
+		setBox(df::Box(df::Vector(-0.45, -0.45), this->length-0.1, 0.9));
 	}
 	else{
-		length = abs(this->end1.getY() - this->end2.getY());
+		length = abs(this->end1.getY() - this->end2.getY()) + 1;
 		dir = 0; // vertical wall
 		if (end1.getY() > end2.getY()) {
 			this->end2.setY(side1.getY());
 			this->end1.setY(side2.getY());
 		}
-		setBox(df::Box(pixelsToSpaces(this->end1 - df::Vector(0.5, 0)), 0.5, this->length - 0.5));
+		setBox(df::Box(df::Vector(-0.45, -0.45), 0.9, this->length-0.1));
 	}
 	setType("Wall");
 	setPosition(end1);
